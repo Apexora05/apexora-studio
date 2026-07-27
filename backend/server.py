a96f62ef51a92e691e64ca52174b0c894e53a5f3
Apexora05 <skullnaturals@gmail.com>
2026-07-27 13:02:47 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -26,7 +26,11 @@ app.include_router(content_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get("CORS_ORIGINS", "*").split(","),
+    allow_origins=[
+    "https://apexora.onrender.com",
+    "http://localhost:3000",
+    "http://localhost:5173",
+],
     allow_methods=["*"],
     allow_headers=["*"],
 )
